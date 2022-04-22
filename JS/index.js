c618bbfdd33b10ef7faf18a1e27c8dd7cfffd079
Martin <mkmartiones@gmail.com>
2022-04-22 22:28:49 +0300

    changes

---
+++ JS/index.js
@@ -0,0 +1,36 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
+// 		'X-RapidAPI-Key': '18e37f9bf1msh17efa5b9b13e64fp1a52d6jsnf1e5561d4a85'
+// 	}
+// };
+
+// fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+// function getRandomQuote(){
+//     const options = {
+//     	method: 'GET',
+//     	headers: {
+//     		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
+//     		'X-RapidAPI-Key': '18e37f9bf1msh17efa5b9b13e64fp1a52d6jsnf1e5561d4a85'
+//     	}
+//     };
+//     // 
+//     fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
+//     	.then(response => response.json())
+//     	.then(response => console.log(response))
+
+// }
+
+
+// setInterval(getRandomQuote, 5000)
+
+// function timer(){
+//     for(let i = 0; i < 20; i++){
+//         getRandomQuote()    
+//     }
+// }
